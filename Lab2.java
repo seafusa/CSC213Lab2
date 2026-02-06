e022a7c070ce5551e7a247f1494f96cd8cfee837
seafusa <ahmedsalmashiakhy@gmail.com>
2026-02-06 11:30:37 -0500

    Created variable

--- Lab2.java
+++ Lab2.java
@@ -4,7 +4,8 @@ import java.math.BigInteger;
 public class Lab2 {
     public static void main(String[] args) {
         System.out.println("Welcome to Lab 2 !");
-        System.out.println(Math.log(0));
+        double logofzero = Math.log(0);
+        System.out.println(logofzero);
         int x = 2000000000;
         int y = 2000000000;
         int z = x + y;
